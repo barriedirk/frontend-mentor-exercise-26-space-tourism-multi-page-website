a6ed2a52674d3ad86548eb2f0b986bfae56a4547
Barrie Freyre <barriedirk@yahoo.com>
2025-06-03 19:53:27 -0500

    change some configuration

--- project/space-tourism/astro.config.mjs
+++ project/space-tourism/astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  // site: "https://barriedirk.github.io",
+  // base: "/frontend-mentor-exercise-26-space-tourism-multi-page-website",
+  compressHTML: false,
+  build: {
+    format: "file",
+    inlineStylesheets: "never",
+  },
   vite: {
     plugins: [tailwindcss()],
-  },
-  experimental: {},
-  build: {
-    inlineStylesheets: "always",
   },
 });
